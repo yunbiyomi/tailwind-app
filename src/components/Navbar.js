ee1693ebdc98cd099fdad55eb5414043f30eea98
yunbiyomi <yunbiyomi@naver.com>
2023-11-17 20:41:24 +0900

    ✨ Feat: 텍스트 클릭시 원하는 곳으로 이동

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { FaTimes, FaAlignJustify } from 'react-icons/fa'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [openToggle, setOpenToggle] = useState(false);
@@ -10,11 +11,11 @@ const Navbar = () => {
         <div className='flex items-center'>
           <h1 className='mr-4 text-3xl font-bold sm:text-4xl'>Discord</h1>
           <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Feature</li>
-            <li>Feedback</li>
+            <li><Link to='home' smooth={true} duration={500}>Home</Link></li>
+            <li><Link to='about' smooth={true} duration={500}>About</Link></li>
+            <li><Link to='support' smooth={true} duration={500}>Support</Link></li>
+            <li><Link to='feature' smooth={true} duration={500}>Feature</Link></li>
+            <li><Link to='feedback' smooth={true} duration={500}>Feedback</Link></li>
           </ul>
         </div>
         <div className='hidden pr-4 md:flex'>
@@ -26,11 +27,11 @@ const Navbar = () => {
         </div>
       </div>
       <ul className={openToggle ? 'absolute bg-zinc-200 w-full px-8' : 'hidden'}>
-        <li className='w-full border-b-2 border-zinc-300'>Home</li>
-        <li className='w-full border-b-2 border-zinc-300'>About</li>
-        <li className='w-full border-b-2 border-zinc-300'>Support</li>
-        <li className='w-full border-b-2 border-zinc-300'>Feature</li>
-        <li className='w-full border-b-2 border-zinc-300'>Feedback</li>
+        <li className='w-full border-b-2 border-zinc-300'><Link onClick={() => setOpenToggle(false)} to='home' smooth={true} duration={500}>Home</Link></li>
+        <li className='w-full border-b-2 border-zinc-300'><Link onClick={() => setOpenToggle(false)} to='about' smooth={true} duration={500}>About</Link></li>
+        <li className='w-full border-b-2 border-zinc-300'><Link onClick={() => setOpenToggle(false)} to='support' smooth={true} duration={500}>Support</Link></li>
+        <li className='w-full border-b-2 border-zinc-300'><Link onClick={() => setOpenToggle(false)} to='feature' smooth={true} duration={500}>Feature</Link></li>
+        <li className='w-full border-b-2 border-zinc-300'><Link onClick={() => setOpenToggle(false)} to='feedback' smooth={true} duration={500}>Feedback</Link></li>
         <div  className='flex flex-col my-4'>
           <button className='px-8 py-3 mb-4 text-slate-600 bg-transparent'>Sign In</button>
           <button className='px-8 py-3'>Sign Up</button>
