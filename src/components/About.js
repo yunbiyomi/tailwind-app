a0e90a8003c55c5f2459117be5a9d99ef158756b
yunbiyomi <yunbiyomi@naver.com>
2023-11-17 16:38:13 +0900

    💄 Style: About Component 구현

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section name="about" className='w-full h-screen'>
+      <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto'>
+        <div className='text-center'>
+          <h2 className='text-5xl font-bold'>About this company</h2>
+          <p className='py-6 text-3xl text-gray-500'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <p className='mt-20 text-xl'>
+          Suscipit eius, explicabo quidem totam tempore et sit blanditiis, tempora alias ad mollitia voluptatem nisi ipsum labore non, impedit dolor voluptates fugit?
+        </p>
+
+        <p className='mt-20 text-xl'>
+          Suscipit eius, explicabo quidem totam tempore et sit blanditiis, tempora alias ad mollitia voluptatem nisi ipsum labore non, impedit dolor voluptates fugit?
+        </p>
+      </div>
+    </section>
   )
 }
 
