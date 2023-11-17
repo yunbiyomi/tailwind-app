dc4758684f6115fd63b4bb1953b8640cd6c26c35
yunbiyomi <yunbiyomi@naver.com>
2023-11-17 15:16:43 +0900

    🎉 Init : 기초 환경 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Support from './components/Support';
+import Feature from './components/Feature';
+import Feedback from './components/Feedback';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Support />
+      <Feature />
+      <Feedback />
+      <Contact />
+      <Footer />
+    </>
+  )
 }
-
-export default App;
