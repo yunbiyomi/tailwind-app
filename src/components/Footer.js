18dd0058ad52da52a32b39f6b4ca34999231987a
yunbiyomi <yunbiyomi@naver.com>
2023-11-17 19:03:00 +0900

    💄 Style: Footer Component 구현

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,77 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter, FaTwitch, FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full px-2 text-gray-300 bg-slate-700'>
+      <div className='max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
+        <div className='text-center'>
+          <h6 className='pt-2 font-bold uppercase'>Solutions</h6>
+          <ul>
+            <li className='py-1'>Marketing</li>
+            <li className='py-1'>Analytucs</li>
+            <li className='py-1'>Cinnerce</li>
+            <li className='py-1'>Data</li>
+          </ul>
+        </div>
+
+        <div className='text-center'>
+          <h6 className='pt-2 font-bold uppercase'>Support</h6>
+          <ul>
+            <li className='py-1'>Pricing</li>
+            <li className='py-1'>Documentation</li>
+            <li className='py-1'>Guides</li>
+            <li className='py-1'>API Status</li>
+          </ul>
+        </div>
+
+        <div className='text-center'>
+          <h6 className='pt-2 font-bold uppercase'>Company</h6>
+          <ul>
+            <li className='py-1'>About</li>
+            <li className='py-1'>Blog</li>
+            <li className='py-1'>Jobs</li>
+          </ul>
+        </div>
+
+        <div className='text-center'>
+          <h6 className='pt-2 font-bold uppercase'>Legal</h6>
+          <ul>
+            <li className='py-1'>Claims</li>
+            <li className='py-1'>Privacy</li>
+            <li className='py-1'>Terms</li>
+            <li className='py-1'>Policies</li>
+            <li className='py-1'>Conditions</li>
+          </ul>
+        </div>
+
+        <div className='col-span-2 pt-8 md:pt-2'>
+          <p className='font-bold uppercase'>Subscribe to our newsletter</p>
+          <p className='py-4'>The latest news, articles, and resourcesm sent to your inbox weekly</p>
+          <form className='flex flex-col sm:flex-row '>
+            <input 
+              type='email'
+              placeholder='Enter email...'
+              className='w-full p-2 mb-4 mr-4 rounded-md'
+            />
+            <button className='p-2 mb-4'>
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className='flex felx-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500'>
+        <p className='py-4'>{new Date().getFullYear()} Discord, LLC. ALl rights reserved</p>
+        <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
+          <FaFacebook />
+          <FaInstagram />
+          <FaTwitter />
+          <FaTwitch />
+          <FaGithub />
+        </div>
+      </div>
+    </footer>
   )
 }
 
